1188aac86c65eea5038fffd69edfa3ef59a3407c
cszsm <zscseh93@gmail.com>
2025-02-18 20:04:14 +0100

    Two basic screens

--- app/src/main/java/cszsm/dolgok/MainActivity.kt
+++ app/src/main/java/cszsm/dolgok/MainActivity.kt
@@ -3,13 +3,7 @@ package cszsm.dolgok
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import cszsm.dolgok.ui.screens.main.MainScreen
 import cszsm.dolgok.ui.theme.DolgokTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DolgokTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DolgokTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/cszsm/dolgok/ui/screens/main/MainScreen.kt
@@ -0,0 +1,27 @@
+package cszsm.dolgok.ui.screens.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier.wrapContentSize()
+        ) {
+            Text(text = "LazyColumn")
+        }
+    }
+}
